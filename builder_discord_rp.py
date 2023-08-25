30dbc095ade6fc9d7508c2f2ad930f7ae04f6b3e
DaemonWhite <matheo.daemon@gmail.com>
2023-08-25 18:25:01 +0200

    Dicord No Connected

--- builder_discord_rp.py
+++ builder_discord_rp.py
@@ -5,7 +5,8 @@ from gi.repository import GObject
 from gi.repository import Ide
 
 from threading import Thread
-from pypresence import Presence 
+from pypresence import Presence
+
 
 class BuilderDiscordPresence(GObject.Object, Ide.ApplicationAddin):
 
@@ -15,15 +16,22 @@ class BuilderDiscordPresence(GObject.Object, Ide.ApplicationAddin):
                 time.sleep(15)
 
     def do_load(self, application):
-        self.__rich_start = True
+        self.__rich_start = False
         self.__client_id = "1144326276816060567"
-        self.__RPC = Presence(self.__client_id)
-        self.__RPC.connect()
-        r_update = Thread(target=self.rich_update)
-        r_update.start()
-        print("hello")
+        try:
+            self.__rich_start = True
+            self.__RPC = Presence(self.__client_id)
+            self.__RPC.connect()
+            r_update = Thread(target=self.rich_update)
+            r_update.start()
+        except:
+            print("Discord no connected")
 
+    def do_language_changed(self, lang_id: str):
+        print("Langage id : ",lang_id)
 
     def do_unload(self, application):
-        self.__rich_start = False
+        if self.__rich_start:
+            self.__RPC.close()
+            self.__rich_start = False
         print("goodbye")
